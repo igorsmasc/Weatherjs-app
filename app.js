dc4182c8e6dfe9643a7434eb94a448bad9bf6a64
Igor Mascarenhas <igorsmascarenhas@outlook.com>
2019-08-09 00:29:12 -0300

    Local and temperature

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-window.addEventListener('load' () => {
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimezone = document.querySelector('.location-timezone');
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
+
+      const proxy = 'https://cors-anywhere.herokuapp.com/'
+      const api = `${proxy}https://api.darksky.net/forecast/87c0a04b449fd11960515b49b3e0fcd2/${lat},${long}`;
+
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+            const {temperature, summary} = data.currently;
+
+            //Set DOM Elements from the API
+
+            temperatureDegree.textContent = temperature;
+            temperatureDescription.textContent = summary;
+            locationTimezone.textContent = data.timezone;
+
+
+        });
+    });
+  } else {
+    h1.textContent = "Hey, please enable your location";
+  }
 });
