f14fd72bc6eb8a61237766c3d7b66be7401e0805
Igor Mascarenhas <igorsmascarenhas@outlook.com>
2019-08-09 01:01:51 -0300

    Finishing the project

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
-  let temperatureDescription = document.querySelector('.temperature-description');
-  let temperatureDegree = document.querySelector('.temperature-degree');
-  let locationTimezone = document.querySelector('.location-timezone');
-  
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".temperature span");
+
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-
-      const proxy = 'https://cors-anywhere.herokuapp.com/'
+      const proxy = "https://cors-anywhere.herokuapp.com/";
       const api = `${proxy}https://api.darksky.net/forecast/87c0a04b449fd11960515b49b3e0fcd2/${lat},${long}`;
 
       fetch(api)
@@ -19,18 +23,45 @@ window.addEventListener("load", () => {
           return response.json();
         })
         .then(data => {
-            const {temperature, summary} = data.currently;
+          const { temperature, summary, icon } = data.currently;
 
-            //Set DOM Elements from the API
+          //Set DOM Elements from the API
 
-            temperatureDegree.textContent = temperature;
-            temperatureDescription.textContent = summary;
-            locationTimezone.textContent = data.timezone;
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
+
+          // Formula for celsius
+          let celsius = (temperature - 32) * (5 / 9);
 
 
+          console.log(icon);
+
+          //Set Icon
+
+          setIcons(icon, document.querySelector(".icon"));
+
+          // Change temperaturo to Celsius/Farenheit
+            temperatureSection.addEventListener('click', ()=> {
+                if(temperatureSpan.textContent == "F") {
+                    temperatureSpan.textContent = "C";
+                    temperatureDegree.textContent = Math.floor(celsius);
+                } else {
+                    temperatureSpan.textContent = "F";
+                    temperatureDegree.textContent = temperature;
+                }
+            })
+            
         });
     });
-  } else {
-    h1.textContent = "Hey, please enable your location";
+  }
+
+  function setIcons(icon, iconId) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    
+    console.log(currentIcon);
+    skycons.play();
+    return skycons.set(iconId, Skycons[currentIcon]);
   }
 });
